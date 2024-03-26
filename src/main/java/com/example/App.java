c020cdc911ba42c9411ae5f1f30c0fbf1fc98a9c
Amrit Kafle <akafle1@toromail.csudh.edu>
2024-03-26 15:50:13 -0700

    set

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        MathUtils utils = new MathUtils();
+        System.out.println(utils.add(2, 4));
+    }
+}
